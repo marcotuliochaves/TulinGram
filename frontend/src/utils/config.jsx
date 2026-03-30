d4543b20829d948b76412d35394f3b3d7b3cfc0d
Marco Túlio <88911215+marcotuliochaves@users.noreply.github.com>
2026-03-30 13:54:05 -0300

    Update API and uploads URLs to production

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -1,5 +1,5 @@
-export const api = "http://localhost:5000/api";
-export const uploads = "http://localhost:5000/uploads";
+export const api = "https://tulingramofc.onrender.com/api";
+export const uploads = "https://tulingramofc.onrender.com/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
   let config;
