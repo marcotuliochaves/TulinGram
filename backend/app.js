fcea9a5bca5512cd9c05abe5c54327aea65454c9
Marco Oliveira <marco.oliveira.chaves23@gmail.com>
2026-03-30 13:42:32 -0300

    Fix: cors

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 // Solve CORS
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://tulingramofc.vercel.app"
+    ],
     credentials: true,
   })
 );
